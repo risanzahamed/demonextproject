ecdceb6bae70ed673dddcf7b215ae182cb6246ff
abusaidshabib <abusaidshabib712@.gmail.com>
2023-05-02 21:34:30 +0600

    Brandlogos solve

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,9 @@
-import BrandLogos from "./components/BrandLogos/BrandLogos";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
-      <BrandLogos></BrandLogos>
+      <Link href="/check" className="bg-primary-700 py-2 px-5 text-white">Check</Link>
     </div>
   )
 }
