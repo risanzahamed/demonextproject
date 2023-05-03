9afea1d9c5d5b7a0ce8fd4a4a7307a6381602c5c
ABUSAIDSHABIB <92622461+abusaidshabib@users.noreply.github.com>
2023-05-03 19:29:59 +0600

    Services page add

--- src/pages/services.tsx
+++ src/pages/services.tsx
@@ -1,1 +1,19 @@
 
+import React from 'react';
+import Services from './components/Service/Services';
+import ServicePrice from './components/Service/ServicePrice';
+import Offer from './components/Service/Offer';
+import WorkingProcess from './components/Service/WorkingProcess/WorkingProcess';
+
+const services = () => {
+  return (
+    <div>
+      <Services></Services>
+      <ServicePrice></ServicePrice>
+      <Offer></Offer>
+     <WorkingProcess></WorkingProcess>
+    </div>
+  );
+};
+
+export default services;
