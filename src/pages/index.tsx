dc6703519cbbed043cbba978dc8a28eb4d83e0a3
BayajidAlam <bayajidalam2001@gmail.com>
2023-05-03 19:06:19 +0600

    footer ready

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 
 export default function Home() {
   return (
@@ -7,6 +8,7 @@ export default function Home() {
       {/* <Link href="/check" className="bg-primary-700 py-2 px-5 text-white">Check</Link> */}
 
       <Header></Header>
+      <Footer/>
     </div>
   )
 }
