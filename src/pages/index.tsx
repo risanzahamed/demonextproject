99561c6d543ca99be246848c9e6f947cad57b726
BayajidAlam <bayajidalam2001@gmail.com>
2023-05-03 19:07:46 +0600

    header footer added on doc.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,10 +5,8 @@ import Footer from "./components/Footer/Footer";
 export default function Home() {
   return (
     <div>
-      {/* <Link href="/check" className="bg-primary-700 py-2 px-5 text-white">Check</Link> */}
+      <Link href="/check" className="bg-primary-700 py-2 px-5 text-white">Check</Link>
 
-      <Header></Header>
-      <Footer/>
     </div>
   )
 }
