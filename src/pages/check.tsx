a17837786ba3de0deeb808ee244b173c3fd467f5
Saima <saimasawrin324@gmail.com>
2023-05-04 00:41:35 +0600

    update

--- src/pages/check.tsx
+++ src/pages/check.tsx
@@ -1,28 +1,15 @@
-<<<<<<< HEAD
-import React from 'react';
-import About from './components/About/About';
-import Location from './components/Location/Location';
-import Services from './components/Service/Services';
+import About from "./components/About/About";
+import Blog from "./components/Blog/Blog";
+import Services from "./components/Service/Services";
 
-import Blog from './components/Blog/Blog';
-
-
-
-const check = () => {
+function check() {
   return (
     <div>
-   
-      <About></About>
-       <Blog></Blog>
-      <Location></Location>
-      <Services></Services>
-=======
-const check = () => {
-  return (
-    <div>
->>>>>>> d8adc534f25257a5b226cc3606c40eb81fae0f49
+     <About></About>
+     <Blog></Blog>
+     <Services></Services>
     </div>
   );
-};
+}
 
 export default check;
