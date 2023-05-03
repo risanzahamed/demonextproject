7997d98a057bbe3dd14e71365017b1ad01d1330f
Saima <saimasawrin324@gmail.com>
2023-05-03 13:08:32 +0600

    About update

--- src/pages/components/About/About.tsx
+++ src/pages/components/About/About.tsx
@@ -10,8 +10,8 @@ function About() {
  
 
 
-      <div className="relative flex flex-col px-4 py-16 gap-20 mx-auto lg:block lg:flex-col lg:py-32 xl:py-48 md: sm:max-w-xl md:max-w-full bg-slate-50">
-        <div className="z-0 flex justify-center h-full -mx-4 overflow-hidden lg:pt-24 lg:pb-16 lg:pr-8 xl:pr-0 lg:w-1/2 lg:absolute lg:justify-end lg:bottom-0 lg:left-0 lg:items-center">
+      <div className="grid grid-cols-2 gap-10">
+        {/* <div className="z-0 flex justify-center h-full -mx-4 overflow-hidden lg:pt-24 lg:pb-16 lg:pr-8 xl:pr-0 lg:w-1/2 lg:absolute lg:justify-end lg:bottom-0 lg:left-0 lg:items-center">
 
           <Image
           
@@ -19,6 +19,16 @@ function About() {
             className="object-left w-full h-auto lg:w-auto lg:h-full"
             alt=""
           />
+        </div> */}
+        <div className="grid place-items-center sm:col-span-1">
+        <Image
+          
+          src={brand}
+          className="object-left w-full h-auto lg:w-auto lg:h-full"
+          alt=""
+          width={500}
+          height={500}
+        />
         </div>
         <div className=" flex justify-end max-w-xl mx-auto xl:pr-32 lg:max-w-screen-xl">
           <div className="lg:pr-5 lg:max-w-lg lg:mb-0">
@@ -32,9 +42,7 @@ function About() {
                 Building Brands with
                 <br className="hidden md:block" />
                Purpose and Passion{' '}
-                {/* <span className="inline-block text-deep-purple-accent-400">
-                  online
-                </span> */}
+              
               </h2>
               <p className="text-base text-gray-700 md:text-lg">
                 Sed ut perspiciatis unde omnis iste natus error sit voluptatem
@@ -43,7 +51,7 @@ function About() {
               </p>
             </div>
             <form>
-              <div className="flex flex-col md:flex-row">
+              <div className="grid">
                 
                 <div>
                   <Image  src={icon1} alt="" className='h-12 w-12' />
