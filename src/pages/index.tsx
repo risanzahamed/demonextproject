6f7120b9c659268a81262641b7b88a2200270121
BayajidAlam <bayajidalam2001@gmail.com>
2023-05-04 00:08:13 +0600

    why choose us

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
 import Hero from "./components/Hero/Hero";
+import WhyChooseUs from "./components/WhyChooseUs/WhyChooseUs";
 
 export default function Home() {
   return (
     <div>
       <Hero/>
+      <WhyChooseUs/>
     </div>
   )
 }
