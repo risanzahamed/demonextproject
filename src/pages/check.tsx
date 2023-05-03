d8adc534f25257a5b226cc3606c40eb81fae0f49
ABUSAIDSHABIB <92622461+abusaidshabib@users.noreply.github.com>
2023-05-03 18:28:33 +0600

    update checked file
    update checked file

--- src/pages/check.tsx
+++ src/pages/check.tsx
@@ -1,14 +1,6 @@
-import React from 'react';
-import About from './components/About/About';
-import Location from './components/Location/Location';
-import Services from './components/Service/Services';
-
 const check = () => {
   return (
     <div>
-      <About></About>
-      <Location></Location>
-      <Services></Services>
     </div>
   );
 };
