096cc0ed551f74fa4fc7cb14f9e0a281572999bf
ABUSAIDSHABIB <92622461+abusaidshabib@users.noreply.github.com>
2023-05-03 18:27:31 +0600

    Update check.tsx

--- src/pages/check.tsx
+++ src/pages/check.tsx
@@ -1,13 +1,6 @@
-import Offer from "./components/Service/Offer";
-import ServicePrice from "./components/Service/ServicePrice";
-import WorkingProcess from "./components/Service/WorkingProcess/WorkingProcess";
-
 const check = () => {
   return (
     <div>
-      <WorkingProcess></WorkingProcess>
-      <ServicePrice></ServicePrice>
-      <Offer></Offer>
     </div>
   );
 };
