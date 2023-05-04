c0cce71b0386edd45a9aeeca0e1e61f0b4e760b3
ABUSAIDSHABIB <92622461+abusaidshabib@users.noreply.github.com>
2023-05-04 08:39:26 +0600

    Update index.tsx
    index clear

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,6 @@
-import Hero from "./components/Hero/Hero";
-import WhyChooseUs from "./components/WhyChooseUs/WhyChooseUs";
-
 export default function Home() {
   return (
     <div>
-      <Hero/>
-      <WhyChooseUs/>
     </div>
   )
 }
