32560d93a1e49b956da6b4dcefd5697bee77bc7c
Risan Ahamed <risanzahamed76@gmail.com>
2023-05-05 13:47:07 +0600

    card

--- src/pages/components/Service/WorkingProcess/WorkingProcess.tsx
+++ src/pages/components/Service/WorkingProcess/WorkingProcess.tsx
@@ -1,10 +1,13 @@
 // import img1 from "../../../assets/icons/budget-removebg-preview.png";
+import Image from "next/image";
 import img1 from "../../../../assets/icons/budget-removebg-preview.png";
 import img2 from "../../../../assets/icons/delivery.png";
 import img3 from "../../../../assets/icons/work3-removebg-preview (1).png";
-import WorkingProcessCard from "./WorkingProcessCard";
-
+import { CSSProperties } from "react";
 const WorkingProcess = () => {
+
+
+
   const works = [
     {
       id: "1",
@@ -29,6 +32,11 @@ const WorkingProcess = () => {
     },
   ];
 
+  const imageStyle: CSSProperties = {
+    width: "200px",
+    height: "180px",
+  };
+
   return (
     <div>
       <div className="text-center">
@@ -40,9 +48,73 @@ const WorkingProcess = () => {
         </p>
       </div>
       <div className="grid lg:grid-cols-3 gap-10 lg:mx-32 lg:mb-32">
-        {works.map((work, id) => (
-          <WorkingProcessCard work={work} key={id}></WorkingProcessCard>
-        ))}
+      
+
+        <div className="w-96 p-5 flex flex-col justify-center items-center hover:border border-yellow-800">
+          <div>
+            <Image
+              src={img1}
+              width={200}
+              height={250}
+              alt=""
+              style={imageStyle}
+              className=""
+            ></Image>
+          </div>
+
+          <div>
+            <h1 className="text-center my-5 lg:my-5 text-xl font-bold">
+              Discussion Budget
+            </h1>
+          </div>
+          <div>
+            <p className="text-center">Over the course of preparing and adopting a budget, the parties involved in the process will have many (many) discussions. These discussions offer opportunities to identify concerns, priorities, and goals, and to make any changes to your policies...</p>
+          </div>
+        </div>
+
+        <div className="w-96 p-5 flex flex-col justify-center items-center hover:border border-yellow-800">
+          <div>
+            <Image
+              src={img2}
+              width={200}
+              height={250}
+              alt=""
+              style={imageStyle}
+              className=""
+            ></Image>
+          </div>
+
+          <div>
+            <h1 className="text-center my-5 lg:my-5 text-xl font-bold">
+              Project Delivery
+            </h1>
+          </div>
+          <div>
+            <p className="text-center">Over the course of preparing and adopting a budget, the parties involved in the process will have many (many) discussions. These discussions offer opportunities to identify concerns, priorities, and goals, and to make any changes to your policies.</p>
+          </div>
+        </div>
+
+        <div className="w-96 p-5 flex flex-col justify-center items-center hover:border border-yellow-800">
+          <div>
+            <Image
+              src={img3}
+              width={200}
+              height={250}
+              alt=""
+              style={imageStyle}
+              className=""
+            ></Image>
+          </div>
+
+          <div>
+            <h1 className="text-center my-5 lg:my-5 text-xl font-bold">
+              Star Your Work
+            </h1>
+          </div>
+          <div>
+            <p className="text-center">Over the course of preparing and adopting a budget, the parties involved in the process will have many (many) discussions. These discussions offer opportunities to identify concerns, priorities, and goals, and to make any changes to your policies...</p>
+          </div>
+        </div>
       </div>
     </div>
   );
